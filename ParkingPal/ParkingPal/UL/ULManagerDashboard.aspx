98103ad3af032819f3348026fa384b6b95159948
csfrank271 <csfrank271@gmail.com>
2017-09-02 15:36:08 +1000

    Add manager page

---
+++ ParkingPal/ParkingPal/UL/ULManagerDashboard.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/ParkingPalMaster.Master" AutoEventWireup="true" CodeBehind="ULManagerDashboard.aspx.cs" Inherits="ParkingPal.UL.ULManagerDashboard" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ ParkingPal/ParkingPal/UL/ULManagerDashboard.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ParkingPal.UL
+{
+    public partial class ULManagerDashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string strNewURL = null;
+
+            try
+            {
+                if (Session["User"] == null)
+                {
+                    throw new Exception("You are not authenticated to access this page.");
+                }
+                else
+                {
+                   
+                }
+            }
+            catch (Exception exception)
+            {
+                strNewURL = "~/UL/ULError.aspx";
+                Session["exception"] = exception;
+            }
+
+            // Redirect to the next page:
+            if (strNewURL != null)
+            {
+                Response.Redirect(strNewURL);
+            }
+        }
+    }
+}
