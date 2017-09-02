2427d2571a89c3d33519b76f20891376cd2488b4
csfrank271 <csfrank271@gmail.com>
2017-09-02 11:10:10 +1000

    Add master page and script to initialise materialize CSS functionality

---
+++ ParkingPal/ParkingPal/JS/initialiseMaterialize.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $(".button-collapse").sideNav();
+});
+$(document).ready(function () {
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
---
+++ ParkingPal/ParkingPal/MasterPages/ParkingPalMaster.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ParkingPal.MasterPages
+{
+    public partial class ParkingPalMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Enforce the application to not store in the cache to resolve some back button issues:
+            Response.Cache.SetNoStore();
+            Response.Cache.AppendCacheExtension("no-cache");
+            Response.Expires = 0;
+
+            if (Session["User"] == null)
+            {
+                unauthenticatedNav.Visible = true;
+            }
+            else
+            {
+                authenticatedNav.Visible = true;
+            }
+        }
+    }
+}
