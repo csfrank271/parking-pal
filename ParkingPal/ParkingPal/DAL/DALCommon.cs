617d31f00456aa6c546392996c4d5ff018d509da
csfrank271 <csfrank271@gmail.com>
2017-09-02 15:36:40 +1000

    Add common DAL layer functions class

---
+++ ParkingPal/ParkingPal/DAL/DALCommon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ParkingPal.DAL
+{
+    public class DALCommon
+    {
+        // Creates a new connection to the database:
+        public static SqlConnection NewConnection()
+        {
+            return new SqlConnection(
+                System.Configuration.ConfigurationManager.ConnectionStrings
+                ["parkingPalDBConnection"].ConnectionString);
+        }
+    }
+}
