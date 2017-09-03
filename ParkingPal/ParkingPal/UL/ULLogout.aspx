155f80e06f371e499da02562823c1490f7a3cdd6
csfrank271 <csfrank271@gmail.com>
2017-09-03 22:40:39 +1000

    Add logout functionality and message for user.

--- ParkingPal/ParkingPal/UL/ULLogout.aspx
+++ ParkingPal/ParkingPal/UL/ULLogout.aspx
@@ -2,4 +2,16 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+                    <p>
+                        You have been successfully logged out.
+                        <a class="" href="ULHome.aspx">Click here to return to the home page</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- ParkingPal/ParkingPal/UL/ULLogout.aspx.cs
+++ ParkingPal/ParkingPal/UL/ULLogout.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ParkingPal.Models;
 
 namespace ParkingPal.UL
 {
@@ -11,7 +12,37 @@ namespace ParkingPal.UL
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string strNewURL = null;
 
+            // Authenticate the user:
+            AppUser appUser = (AppUser)Session["AppUser"];
+            try
+            {
+                string pageURL = HttpContext.Current.Request.Url.AbsolutePath;
+                string redirect = Authenticator.AuthenticateUser(appUser, pageURL);
+                if (redirect != null)
+                {
+                    strNewURL = "~" + redirect;
+                }
+                else
+                {
+                    // Clear all session data to reset application to user:
+                    Session.Abandon();
+                }
+            }
+            catch (Exception exception)
+            {
+                strNewURL = "~/UL/ULError.aspx";
+                Session["exception"] = exception;
+            }
+            finally
+            {
+                // Redirect to the next page:
+                if (strNewURL != null)
+                {
+                    Response.Redirect(strNewURL);
+                }
+            }
         }
     }
 }
