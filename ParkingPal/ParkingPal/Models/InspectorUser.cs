db41e9c9a38526c6d7952f75e0c9beff5bf007d8
csfrank271 <csfrank271@gmail.com>
2017-09-09 08:56:03 +1000

    Add InspectorUser model. This model binds an AppUser to an Inspector and allows us to collate complete Inspector  information in a single SQL stored procedure.

---
+++ ParkingPal/ParkingPal/Models/InspectorUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingPal.Models
+{
+    public class InspectorUser
+    {
+        private AppUser appUser;
+        private Inspector inspector;
+
+        // Constructor
+        public InspectorUser(AppUser appUser, Inspector inspector)
+        {
+            this.appUser = appUser;
+            this.inspector = inspector;
+        }
+
+        // Properties
+        public AppUser AppUser
+        {
+            get
+            {
+                return appUser;
+            }
+
+            set
+            {
+                appUser = value;
+            }
+        }
+
+        public Inspector Inspector
+        {
+            get
+            {
+                return inspector;
+            }
+
+            set
+            {
+                inspector = value;
+            }
+        }
+    }
+}
