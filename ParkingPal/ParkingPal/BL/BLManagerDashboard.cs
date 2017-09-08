5933d7f89d43236a721ae8f6402ca5a1c116925f
csfrank271 <csfrank271@gmail.com>
2017-09-09 08:54:58 +1000

    Add BL and DAL layers for ManagerDashboard and function for retrieving the inspectors for a manager.

---
+++ ParkingPal/ParkingPal/BL/BLManagerDashboard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ParkingPal.DAL;
+using ParkingPal.Models;
+
+namespace ParkingPal.BL
+{
+    public class BLManagerDashboard
+    {
+        public static List<InspectorUser> GetManagerInspectors (int managerID)
+        {
+            try
+            {
+                return DALManagerDashboard.GetManagerInspectors(managerID);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
---
+++ ParkingPal/ParkingPal/DAL/DALManagerDashboard.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ParkingPal.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ParkingPal.DAL
+{
+    public class DALManagerDashboard
+    {
+        // Retrieves an appUser by searching for one with a matching userName and userPassword.
+        public static List<InspectorUser> GetManagerInspectors(int managerID)
+        {
+            // Initialise appUser:
+            List<InspectorUser> inspectorUsers = null;
+
+            try
+            {
+                using (SqlConnection sqlConn = DALCommon.NewConnection())
+                {
+                    // Set the SQL command and its parameters
+                    SqlCommand sqlComm = new SqlCommand("dbo.sp_get_inspectors_for_manager", sqlConn);
+                    sqlComm.CommandType = CommandType.StoredProcedure;
+                    sqlComm.Parameters.Add("@manager_id", SqlDbType.Int, 50).Value = managerID;
+
+                    // Open the SQL connection and run the command:
+                    sqlConn.Open();
+                    sqlComm.ExecuteNonQuery();
+                    SqlDataReader reader = sqlComm.ExecuteReader();
+
+                    // Retrieve the record if it exists:
+                    if(reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            AppUser appUser = new AppUser
+                            (
+                                (int)reader["AppUserID"],
+                                reader["UserName"].ToString(),
+                                reader["UserPassword"].ToString(),
+                                reader["FirstName"].ToString(),
+                                reader["LastName"].ToString(),
+                                reader["AppUserType"].ToString().ToCharArray()[0]
+                            );
+                            Inspector inspector = new Inspector
+                            (
+                                (int)reader["InspectorID"],
+                                (int)reader["AppUserID"],
+                                (int)reader["Manager"]
+                            );
+                            inspectorUsers.Add(new InspectorUser(appUser, inspector));
+                        }
+                    }
+                    
+                    // Close the reader:
+                    reader.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+
+            return inspectorUsers;
+        }
+    }
+}
