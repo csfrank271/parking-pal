d25b614165c7ea6a54b841eea775af21e35d78d7
csfrank271 <csfrank271@gmail.com>
2017-09-10 20:01:13 +1000

    Update ParkingLot model to accomdate for new parking types.

--- ParkingPal/ParkingPal/Models/ParkingLot.cs
+++ ParkingPal/ParkingPal/Models/ParkingLot.cs
@@ -7,43 +7,23 @@ namespace ParkingPal.Models
 {
     public class ParkingLot
     {
-        private int parkingLotID, totalCarparks, functioningCarparks;
+        private int parkingLotID, totalCarparks, generalParkingSpots,
+            disabledParkingSpots, carpoolParkingSpots, serviceParkingSpots,
+            positionParkingSpots, reservedParkingSpots, visitorsParkingSpots;
         private int? adminID;
         private char approvalStatus;
-        private string address, coordinates;
-
-        public ParkingLot(int parkingLotID, int? adminID, int totalCarparks, int functioningCarparks, char approvalStatus, string address, string coordinates)
-        {
-            this.parkingLotID = parkingLotID;
-            this.adminID = adminID;
-            this.totalCarparks = totalCarparks;
-            this.functioningCarparks = functioningCarparks;
-            this.approvalStatus = approvalStatus;
-            this.address = address;
-            this.coordinates = coordinates;
-        }
+        private string shortName, address, coordinates;
 
         public int ParkingLotID
         {
             get
             {
-                return this.parkingLotID;
+                return parkingLotID;
             }
-            set
-            {
-                this.parkingLotID = value;
-            }
-        }
 
-        public int? AdminID
-        {
-            get
-            {
-                return this.adminID;
-            }
             set
             {
-                this.adminID = value;
+                parkingLotID = value;
             }
         }
 
@@ -51,56 +31,192 @@ namespace ParkingPal.Models
         {
             get
             {
-                return this.totalCarparks;
+                return totalCarparks;
             }
+
             set
             {
-                this.totalCarparks = value;
+                totalCarparks = value;
             }
         }
-        public int FunctioningCarparks
+
+        public int GeneralParkingSpots
         {
             get
             {
-                return this.functioningCarparks;
+                return generalParkingSpots;
             }
+
             set
             {
-                this.functioningCarparks = value;
+                generalParkingSpots = value;
             }
         }
+
+        public int DisabledParkingSpots
+        {
+            get
+            {
+                return disabledParkingSpots;
+            }
+
+            set
+            {
+                disabledParkingSpots = value;
+            }
+        }
+
+        public int CarpoolParkingSpots
+        {
+            get
+            {
+                return carpoolParkingSpots;
+            }
+
+            set
+            {
+                carpoolParkingSpots = value;
+            }
+        }
+
+        public int ServiceParkingSpots
+        {
+            get
+            {
+                return serviceParkingSpots;
+            }
+
+            set
+            {
+                serviceParkingSpots = value;
+            }
+        }
+
+        public int PositionParkingSpots
+        {
+            get
+            {
+                return positionParkingSpots;
+            }
+
+            set
+            {
+                positionParkingSpots = value;
+            }
+        }
+
+        public int ReservedParkingSpots
+        {
+            get
+            {
+                return reservedParkingSpots;
+            }
+
+            set
+            {
+                reservedParkingSpots = value;
+            }
+        }
+
+        public int VisitorsParkingSpots
+        {
+            get
+            {
+                return visitorsParkingSpots;
+            }
+
+            set
+            {
+                visitorsParkingSpots = value;
+            }
+        }
+
+        public int? AdminID
+        {
+            get
+            {
+                return adminID;
+            }
+
+            set
+            {
+                adminID = value;
+            }
+        }
+
         public char ApprovalStatus
         {
             get
             {
-                return this.approvalStatus;
+                return approvalStatus;
             }
+
             set
             {
-                this.approvalStatus = value;
+                approvalStatus = value;
             }
         }
+
+        public string ShortName
+        {
+            get
+            {
+                return shortName;
+            }
+
+            set
+            {
+                shortName = value;
+            }
+        }
+
         public string Address
         {
             get
             {
-                return this.address;
+                return address;
             }
+
             set
             {
-                this.address = value;
+                address = value;
             }
         }
+
         public string Coordinates
         {
             get
             {
-                return this.coordinates;
+                return coordinates;
             }
+
             set
             {
-                this.coordinates = value;
+                coordinates = value;
             }
+        }
+
+        public ParkingLot(int parkingLotID, int totalCarparks,
+            int generalParkingSpots, int disabledParkingSpots,
+            int carpoolParkingSpots, int serviceParkingSpots,
+            int positionParkingSpots, int reservedParkingSpots,
+            int visitorsParkingSpots, int? adminID, char approvalStatus,
+            string shortName, string address, string coordinates)
+        {
+            this.ParkingLotID = parkingLotID;
+            this.TotalCarparks = totalCarparks;
+            this.GeneralParkingSpots = generalParkingSpots;
+            this.DisabledParkingSpots = disabledParkingSpots;
+            this.CarpoolParkingSpots = carpoolParkingSpots;
+            this.ServiceParkingSpots = serviceParkingSpots;
+            this.PositionParkingSpots = positionParkingSpots;
+            this.ReservedParkingSpots = reservedParkingSpots;
+            this.VisitorsParkingSpots = visitorsParkingSpots;
+            this.AdminID = adminID;
+            this.ApprovalStatus = approvalStatus;
+            this.ShortName = shortName;
+            this.Address = address;
+            this.Coordinates = coordinates;
         }
     }
 }
