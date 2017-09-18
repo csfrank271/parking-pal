b3ba6f401cc6110d3d3ad3de568224a377b6251c
chantellegreen <c3214209@uon.edu.au>
2017-09-18 17:07:54 +1000

    Unbreak shit

--- ParkingPal/ParkingPal/UL/ULRegister.aspx
+++ ParkingPal/ParkingPal/UL/ULRegister.aspx
@@ -7,29 +7,4 @@
         <div class="bgimg" style="background-image: url('../IMG/signa 2010.jpg');">
             <h2 class="home"><%: Page.Title %></h2>
         </div>
-    <!-- Form input fields -->
-        <div class="row">
-            <div class="input-field col m6 s12">
-                <input runat="server" id="inputName" type="text" class="validate">
-                <label for="inputUserName">User Name</label>
-                <asp:RequiredFieldValidator
-                    ID="rfvUserName"
-                    ControlToValidate="inputUserName"
-                    Display="Dynamic"
-                    runat="server"
-                    ErrorMessage="User name can not be empty.">
-                </asp:RequiredFieldValidator>
-            </div>
-            <div class="input-field col m6 s12">
-                <input runat="server" id="inputPassword" type="password" class="validate">
-                <label for="inputPassword">Password</label>
-                <asp:RequiredFieldValidator
-                    ID="rfvPassword"
-                    ControlToValidate="inputPassword"
-                    Display="Dynamic"
-                    runat="server"
-                    ErrorMessage="Password can not be empty.">
-                </asp:RequiredFieldValidator>
-            </div>
-        </div>
 </asp:Content>
--- ParkingPal/ParkingPal/UL/ULRegister.aspx.designer.cs
+++ ParkingPal/ParkingPal/UL/ULRegister.aspx.designer.cs
@@ -11,41 +11,5 @@ namespace ParkingPal.UL {
     
     
     public partial class ULRegister {
-        
-        /// <summary>
-        /// inputName control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText inputName;
-        
-        /// <summary>
-        /// rfvUserName control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvUserName;
-        
-        /// <summary>
-        /// inputPassword control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputPassword inputPassword;
-        
-        /// <summary>
-        /// rfvPassword control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvPassword;
     }
 }
