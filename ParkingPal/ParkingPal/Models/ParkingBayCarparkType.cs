a027c451cfd70bff3cb025fa00412044b4cdb693
csfrank271 <csfrank271@gmail.com>
2017-10-30 15:17:30 +1100

    Add ParkingBayCarparkType model (maps a ParkingBay to a CarparkType)

---
+++ ParkingPal/ParkingPal/Models/ParkingBayCarparkType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingPal.Models
+{
+    public class ParkingBayCarparkType
+    {
+        // Properties
+        int carparkTypeID;
+        string carparkType;
+        int? parkingLotID, parkingBayCarparkTypeID, numberOfParks;
+        public int CarparkTypeID { get; set; }
+        public string CarparkType { get; set; }
+        public int? ParkingLotID { get; set; }
+        public int? ParkingBayCarparkTypeID { get; set; }
+        public int? NumberOfParks { get; set; }
+
+        // Constructor
+        public ParkingBayCarparkType(int carparkTypeID, string carparkType,
+            int? parkingLotID, int? parkingBayCarparkTypeID, int? numberOfParks)
+        {
+            this.CarparkTypeID = carparkTypeID;
+            this.CarparkType = carparkType;
+            this.ParkingLotID = parkingLotID;
+            this.ParkingBayCarparkTypeID = parkingBayCarparkTypeID;
+            this.NumberOfParks = numberOfParks;
+        }
+    }
+}
